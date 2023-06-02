b79a4d4553a4e33a071a2f680c568944094e95a2
장호빈 <devhobin@gmail.com>
2023-06-03 01:15:13 +0900

    feat: 체크박스 UI 상태 추가

--- src/machine/checkbox/checkbox.machine.ts
+++ src/machine/checkbox/checkbox.machine.ts
@@ -1,3 +1,72 @@
-import { createMachine } from 'xstate'
+import { assign, createMachine } from 'xstate'
 
-export const machine = createMachine({})
+export const machine = createMachine(
+  {
+    id: 'Checkbox',
+    initial: 'idle',
+    states: {
+      idle: {},
+    },
+    on: {
+      SET_HOVERED: {
+        actions: 'setIsHovered',
+      },
+      SET_ACTIVE: {
+        actions: 'setIsActive',
+      },
+      SET_FOCUSED: {
+        actions: 'setIsFocused',
+      },
+      SET_DISABLED: {
+        actions: 'setIsDisabled',
+      },
+    },
+    types: {
+      context: {} as {
+        isHovered: boolean
+        isFocused: boolean
+        isActive: boolean
+        isDisabled: boolean
+      },
+      events: {} as
+        | { type: 'SET_HOVERED'; value: boolean }
+        | { type: 'SET_ACTIVE'; value: boolean }
+        | { type: 'SET_FOCUSED'; value: boolean }
+        | { type: 'SET_DISABLED'; value: boolean },
+    },
+    context: ({ input }) => ({
+      isHovered: input.isHovered ?? false,
+      isFocused: input.isFocused ?? false,
+      isActive: input.isActive ?? false,
+      isDisabled: input.isDisabled ?? false,
+    }),
+  },
+  {
+    actions: {
+      setIsHovered: assign(({ event }) => {
+        if (event.type !== 'SET_HOVERED') throw `잘못된 이벤트 타입입니다 - ${event.type}`
+        return {
+          isHovered: event.value,
+        }
+      }),
+      setIsFocused: assign(({ event }) => {
+        if (event.type !== 'SET_FOCUSED') throw `잘못된 이벤트 타입입니다 - ${event.type}`
+        return {
+          isFocused: event.value,
+        }
+      }),
+      setIsActive: assign(({ event }) => {
+        if (event.type !== 'SET_ACTIVE') throw `잘못된 이벤트 타입입니다 - ${event.type}`
+        return {
+          isActive: event.value,
+        }
+      }),
+      setIsDisabled: assign(({ event }) => {
+        if (event.type !== 'SET_DISABLED') throw `잘못된 이벤트 타입입니다 - ${event.type}`
+        return {
+          isDisabled: event.value,
+        }
+      }),
+    },
+  },
+)
--- src/ui/checkbox/checkbox.tsx
+++ src/ui/checkbox/checkbox.tsx
@@ -1,10 +1,36 @@
 import { useActor } from '@xstate/react'
-import { forwardRefWithAsChild } from '@/core/forward-ref-with-as-child'
+import { forwardRefWithAsChild } from '@/core/utils/forward-ref-with-as-child'
 import { Dive } from '@/core/dive'
 import { machine } from '@/machine/checkbox'
 
-export const Checkbox = forwardRefWithAsChild<'div'>((props, ref) => {
-  const [state, send] = useActor(machine)
+export const Checkbox = forwardRefWithAsChild<'input'>((props, ref) => {
+  const [state, send] = useActor(machine.provide({}), {
+    input: {
+      isDisabled: props.disabled,
+    },
+  })
 
-  return <Dive.div {...props} ref={ref} />
+  const { context } = state
+  const isFocused = context.isFocused
+  const isHovered = context.isHovered
+  const isActive = context.isActive
+  const isDisabled = context.isDisabled
+
+  return (
+    <Dive.input
+      {...props}
+      ref={ref}
+      onFocus={() => send({ type: 'SET_FOCUSED', value: true })}
+      onBlur={() => send({ type: 'SET_FOCUSED', value: false })}
+      onPointerEnter={() => send({ type: 'SET_HOVERED', value: true })}
+      onPointerLeave={() => send({ type: 'SET_HOVERED', value: false })}
+      onPointerDown={() => send({ type: 'SET_ACTIVE', value: true })}
+      onPointerUp={() => send({ type: 'SET_ACTIVE', value: false })}
+      disabled={isDisabled || undefined}
+      data-dive-focused={isFocused ? '' : undefined}
+      data-dive-hovered={isHovered ? '' : undefined}
+      data-dive-active={isActive ? '' : undefined}
+      data-dive-disabled={isDisabled ? '' : undefined}
+    />
+  )
 })
