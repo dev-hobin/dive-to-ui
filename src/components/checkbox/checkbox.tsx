fe1d2735d14f80c81630e4e25b5bb0713252e1c2
장호빈 <devhobin@gmail.com>
2023-06-09 20:45:32 +0900

    reset: 체크박스 작업 리셋

--- src/components/checkbox/checkbox.tsx
+++ src/components/checkbox/checkbox.tsx
@@ -5,14 +5,7 @@ import { Checkbox as DiveCheckbox } from '@/ui/checkbox'
 
 type CheckboxProps = InputHTMLAttributes<HTMLInputElement>
 export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>((props, ref) => {
-  const [checked, setChecked] = useState<boolean>(false)
-
-  return (
-    <>
-      <button onClick={() => setChecked(!checked)}>상태 : {String(checked)}</button>
-      <DiveCheckbox checked={checked} />
-    </>
-  )
+  return <DiveCheckbox ref={ref} {...props} />
 })
 
 Checkbox.displayName = 'Checkbox'
