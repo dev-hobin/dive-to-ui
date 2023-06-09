c75b671114d0f7a251839ba17aadb4b42e665423
장호빈 <devhobin@gmail.com>
2023-06-10 01:05:30 +0900

    feat: onClick 이벤트를 onChange 이벤트로 이동시킴

--- src/ui/checkbox/checkbox.tsx
+++ src/ui/checkbox/checkbox.tsx
@@ -3,18 +3,17 @@ import { forwardRefWithAsChild } from '@/utils/forward-ref-with-as-child'
 import { Dive } from '@/core/dive'
 import { machine } from '@/machines/checkbox'
 
-export const Checkbox = forwardRefWithAsChild<'input'>((props, forwardedRef) => {
+export const Checkbox = forwardRefWithAsChild<
+  'input',
+  { onCheckedChange?: (checked: boolean) => void }
+>((props, forwardedRef) => {
   const [state, send] = useActor(machine)
-
-  console.log('context', state.context)
-
-  const isChecked = state.context.checked
 
   return (
     <Dive.input
       {...props}
       type="checkbox"
-      onClick={() => send({ type: 'CHECKED.SET', params: { checked: !isChecked } })}
+      onChange={(e) => send({ type: 'CHECKED.SET', params: { checked: e.target.checked } })}
       ref={forwardedRef}
     />
   )
