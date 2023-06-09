6a39808b51b605802990443ad48e2ed1d1d4f7bf
장호빈 <devhobin@gmail.com>
2023-06-10 01:13:46 +0900

    remove: onCheckedChange prop 제거

--- src/ui/checkbox/checkbox.tsx
+++ src/ui/checkbox/checkbox.tsx
@@ -3,10 +3,7 @@ import { forwardRefWithAsChild } from '@/utils/forward-ref-with-as-child'
 import { Dive } from '@/core/dive'
 import { machine } from '@/machines/checkbox'
 
-export const Checkbox = forwardRefWithAsChild<
-  'input',
-  { onCheckedChange?: (checked: boolean) => void }
->((props, forwardedRef) => {
+export const Checkbox = forwardRefWithAsChild<'input'>((props, forwardedRef) => {
   const [state, send] = useActor(machine)
 
   return (
