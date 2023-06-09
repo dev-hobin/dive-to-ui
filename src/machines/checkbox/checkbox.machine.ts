cce4061040fde52461def9e9510703658224056f
장호빈 <devhobin@gmail.com>
2023-06-09 21:10:55 +0900

    feat: 체크박스 머신 컨텍스트 추가

--- src/machines/checkbox/checkbox.machine.ts
+++ src/machines/checkbox/checkbox.machine.ts
@@ -6,4 +6,20 @@ export const machine = createMachine({
   states: {
     idle: {},
   },
+  types: {
+    context: {} as {
+      checked: boolean
+      disabled: boolean
+      required: boolean
+      value: string
+      name: string
+    },
+  },
+  context: ({ input }) => ({
+    checked: input.checked ?? false,
+    disabled: input.disabled ?? false,
+    required: input.required ?? false,
+    name: input.name ?? '',
+    value: input.value ?? 'on',
+  }),
 })
