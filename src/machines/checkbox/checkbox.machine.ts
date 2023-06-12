94d79b569d885095b3a8c57a4e685728b8326e2c
장호빈 <devhobin@gmail.com>
2023-06-12 17:28:01 +0900

    fix: 체크박스 머신 로직 전면 수정

--- src/machines/checkbox/checkbox.machine.ts
+++ src/machines/checkbox/checkbox.machine.ts
@@ -1,45 +1,127 @@
-import { raise, and, assign, createMachine, not } from 'xstate'
+import { assign, createMachine } from 'xstate'
+
+export type CheckedState = 'checked' | 'unchecked' | 'indeterminate'
 
 export const machine = createMachine(
   {
     id: 'Checkbox',
-    initial: 'idle',
+    initial: 'init',
     states: {
-      idle: {},
+      init: {
+        always: [
+          {
+            target: 'disabled',
+            guard: 'isDisabled',
+          },
+          {
+            target: 'checkedState',
+          },
+          {
+            target: '#Checkbox.checkedState.checked',
+            guard: 'isChecked',
+          },
+          {
+            target: '#Checkbox.checkedState.indeterminate',
+            guard: 'isIndeterminate',
+          },
+        ],
+      },
+      checkedState: {
+        initial: 'unchecked',
+        states: {
+          checked: {
+            entry: assign({ checkedState: 'checked' }),
+            exit: assign({ previousCheckedState: 'checked' }),
+            on: {
+              CHECK: {
+                target: 'unchecked',
+              },
+            },
+          },
+          unchecked: {
+            entry: assign({ checkedState: 'unchecked' }),
+            exit: assign({ previousCheckedState: 'unchecked' }),
+            on: {
+              CHECK: {
+                target: 'checked',
+              },
+            },
+          },
+          indeterminate: {
+            entry: assign({ checkedState: 'indeterminate' }),
+            exit: assign({ previousCheckedState: 'indeterminate' }),
+            on: {
+              SET_PREVIOUS_STATE: [
+                {
+                  target: 'checked',
+                  guard: 'isPreviousChecked',
+                },
+                {
+                  target: 'unchecked',
+                  guard: 'isPreviousUnchecked',
+                },
+                {
+                  target: 'indeterminate',
+                  reenter: true,
+                },
+              ],
+            },
+          },
+        },
+        on: {
+          SET_CHECKED: {
+            target: '.checked',
+          },
+          SET_UNCHECKED: {
+            target: '.unchecked',
+          },
+          SET_INDETERMINATE: {
+            target: '.indeterminate',
+          },
+        },
+      },
+      disabled: {
+        entry: assign({ disabled: true }),
+        exit: assign({ disabled: false }),
+        on: {
+          SET_ENABLED: {
+            target: 'init',
+          },
+        },
+      },
     },
-    // @ts-ignore
     on: {
-      'CHECKED.SET': {
-        guard: not('isDisabled'),
-        actions: 'setChecked',
+      SET_DISABLED: {
+        target: '.disabled',
       },
     },
     types: {
       context: {} as {
-        checked: boolean
+        checkedState: CheckedState | null
+        previousCheckedState: CheckedState | null
         disabled: boolean
-        required: boolean
-        value: string
-        name: string
       },
-      events: {} as { type: 'CHECKED.SET'; payload: { checked: boolean } },
+      events: {} as
+        | { type: 'CHECK' }
+        | { type: 'SET_CHECKED' }
+        | { type: 'SET_UNCHECKED' }
+        | { type: 'SET_INDETERMINATE' }
+        | { type: 'SET_DISABLED' }
+        | { type: 'SET_PREVIOUS_STATE' }
+        | { type: 'SET_ENABLED' },
     },
     context: ({ input }) => ({
-      checked: input?.checked ?? false,
+      checkedState: input?.checkedState ?? null,
+      previousCheckedState: null,
       disabled: input?.disabled ?? false,
-      required: input?.required ?? false,
-      name: input?.name ?? '',
-      value: input?.value ?? 'on',
     }),
   },
   {
-    actions: {
-      setChecked: assign(({ event, context }) => {
-        if (event.type !== 'CHECKED.SET') return context
-        return { checked: event.payload.checked }
-      }),
-    },
     guards: {
+      isChecked: ({ context }) => context.checkedState === 'checked',
+      isPreviousChecked: ({ context }) => context.previousCheckedState === 'checked',
+      isPreviousUnchecked: ({ context }) => context.previousCheckedState === 'unchecked',
+      isIndeterminate: ({ context }) => context.checkedState === 'indeterminate',
       isDisabled: ({ context }) => context.disabled,
     },
   },
